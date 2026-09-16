c7c6db1a027ac9e8365910d9afd520aa180dccd8
mat <kijung4041@gmail>
2026-09-16 17:54:33 +0900

    Fix: Set relative base path to resolve 404 asset loading error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // GitHub Pages 배포를 위한 저장소 이름 지정
+  base: './',
+});
